7e714e4428249dcc57dd522b09cf1c86915cedbc
ayaka.kitabora <ayaka.kitabora@theport.jp>
2020-04-02 16:45:20 +0900

    一旦3目並べ完成

--- App.tsx
+++ App.tsx
@@ -1,34 +1,53 @@
 import React, { Component }  from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
 
-class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
-    return (
-      <View style={styles.square}>
-        <Button
-        onPress={() => {Alert.alert('click')}}
-        title={`${this.props.value}`}
-        color="#000"
-        style={styles.button}
-        />
-      </View>
-    );
-  }
+function Square(props) {
+  return (
+    <TouchableOpacity
+      onPress={() => props.onPress()}
+    >
+    <View style={styles.square}>
+      <Text style={styles.squareText}>{props.value}</Text>
+    </View>
+    </TouchableOpacity>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onPress={() => this.handleClick(i)}
+      />
+    );
   }
   render() {
-    status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <View style={styles.container}>
@@ -51,6 +70,26 @@ class Board extends React.Component {
      </View>
     );
   }
+}
+// ヘルパー関数(あとでロジック差し替えたい)
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default class Game extends Component {
@@ -81,6 +120,11 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderWidth: 1,
+  },
+  squareText: {
+    fontSize: 60,
+    textAlign: 'center',
+    lineHeight: 100,
   },
   button: {
     width: 100,
